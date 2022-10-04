be808bf7a14cdb53e61a2649ed1e86477ca9f94d
ad8077 <92010428+ad8077@users.noreply.github.com>
2022-10-04 23:40:42 +0530

    Time: 8 ms (73.12%), Space: 7.3 MB (66.58%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string>&board,int n)
+  /*  bool isSafe(int row,int col,vector<string>&board,int n)
     {
         int duprow=row;
         int dupcol=col;
@@ -48,5 +48,44 @@ public:
             board[i]=s;        }
         solve(0,board,ans,n);
         return ans;
+    }*/
+    
+    
+    //using hashing lesser time complexity than above solution
+    void solve (int col,vector<int>&leftrow,vector<int>&upperdiagonal,vector<int>&lowerdiagonal,vector<vector<string>>&ans,vector<string>&board,int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(leftrow[row]==0&&upperdiagonal[n-1+col-row]==0&&lowerdiagonal[col+row]==0){
+                board[row][col]='Q';
+                leftrow[row]=1;
+                upperdiagonal[n-1+col-row]=1;
+                lowerdiagonal[col+row]=1;
+                solve(col+1,leftrow,upperdiagonal,lowerdiagonal,ans,board,n);
+                 board[row][col]='.';
+                leftrow[row]=0;
+                upperdiagonal[n-1+col-row]=0;
+                lowerdiagonal[col+row]=0;
+                
+            }
+        }
+        
+        
     }
+    
+      vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n);
+        vector<vector<string>>ans;
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;        }
+          vector<int>leftrow(n,0);
+          vector<int>upperdiagonal(2*n-1,0);
+          vector<int>lowerdiagonal(2*n-1,0);
+          solve(0,leftrow,upperdiagonal,lowerdiagonal,ans,board,n);
+          return ans;
+      }    
+    
 };
